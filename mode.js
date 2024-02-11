36541e12c96c746c920e9c754d96626716aab65b
PrithviKushwah <kushwahprithvi78@gmail.com>
2024-02-11 18:28:16 +0530

    mode

--- mode.js
+++ mode.js
@@ -1,6 +1,7 @@
 const btn = document.querySelector(".btn-toggle");
 const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
 const image = document.getElementById("abt-btm-img");
+console.log(image);
 
 const currentTheme = localStorage.getItem("theme");
 if (currentTheme === "dark") {
@@ -8,7 +9,7 @@ if (currentTheme === "dark") {
   image.src = "assets/aboutBottom.png"; // Change this to the path of your dark theme image
 } else if (currentTheme === "light") {
   document.body.classList.toggle("light-theme");
-  image.src = "assets/aboutBottom.png"; // Change this to the path of your light theme image
+  image.src = "assets/abtbottomlite.png"; // Change this to the path of your light theme image
 }
 
 btn.addEventListener("click", function () {
@@ -17,13 +18,13 @@ btn.addEventListener("click", function () {
     var theme = document.body.classList.contains("light-theme")
       ? "light"
       : "dark";
-    image.src = theme === "light" ? "light-image.jpg" : "dark-image.jpg";
+    image.src = theme === "light" ? "assets/aboutBottom.png" : "assets/abtbottomlite.png";
   } else {
     document.body.classList.toggle("dark-theme");
     var theme = document.body.classList.contains("dark-theme")
       ? "dark"
       : "light";
-    image.src = theme === "dark" ? "dark-image.jpg" : "light-image.jpg";
+    image.src = theme === "dark" ? "assets/abtbottomlite.png" : "assets/aboutBottom.png";
   }
   localStorage.setItem("theme", theme);
 });
