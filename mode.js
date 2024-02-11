66f85a298c7683e196c997466b2d6fc2be39ffe2
PrithviKushwah <kushwahprithvi78@gmail.com>
2024-02-11 18:18:46 +0530

    testing

--- mode.js
+++ mode.js
@@ -1,11 +1,14 @@
 const btn = document.querySelector(".btn-toggle");
 const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+const image = document.getElementById("abt-btm-img");
 
 const currentTheme = localStorage.getItem("theme");
-if (currentTheme == "dark") {
+if (currentTheme === "dark") {
   document.body.classList.toggle("dark-theme");
-} else if (currentTheme == "light") {
+  image.src = "assets/aboutBottom.png"; // Change this to the path of your dark theme image
+} else if (currentTheme === "light") {
   document.body.classList.toggle("light-theme");
+  image.src = "assets/aboutBottom.png"; // Change this to the path of your light theme image
 }
 
 btn.addEventListener("click", function () {
@@ -14,11 +17,13 @@ btn.addEventListener("click", function () {
     var theme = document.body.classList.contains("light-theme")
       ? "light"
       : "dark";
+    image.src = theme === "light" ? "light-image.jpg" : "dark-image.jpg";
   } else {
     document.body.classList.toggle("dark-theme");
     var theme = document.body.classList.contains("dark-theme")
       ? "dark"
       : "light";
+    image.src = theme === "dark" ? "dark-image.jpg" : "light-image.jpg";
   }
   localStorage.setItem("theme", theme);
 });
