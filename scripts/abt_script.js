d2f61d35b9f8b39e32e3157c718b4a7785b91133
sanskarkakde14 <sanskarkakde13@gmail.com>
2024-02-27 02:14:20 +0530

    ca

--- scripts/abt_script.js
+++ scripts/abt_script.js
@@ -2,17 +2,19 @@ const btn = document.querySelector(".btn-toggle");
 const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
 const abt_btm_img = document.getElementById("abt-btm-img");
 const abt_line = document.getElementById("abt-line");
-
+const abt_bg=document.getElementById("abt-bg")
 
 const currentTheme = localStorage.getItem("theme");
 if (currentTheme === "dark") {
   abt_btm_img.src = "assets/abtbottomlite.png"; 
   abt_line.src = "assets/abtlinelight.png";
+  abt_bg.src="assets/abtbg-dark.png";
   document.body.classList.toggle("dark-theme");
 } else if (currentTheme === "light") {
   document.body.classList.toggle("light-theme");
   abt_btm_img.src = "assets/aboutBottom.png"; 
   abt_line.src = "assets/abtline.png";
+  abt_bg.src = "assets/abtbg.png";
 }
 
 btn.addEventListener("click", function () {
@@ -23,6 +25,7 @@ btn.addEventListener("click", function () {
       : "dark";
       abt_btm_img.src = theme === "light" ? "assets/aboutBottom.png" : "assets/abtbottomlite.png";
       abt_line.src = theme === "light" ? "assets/abtline.png" : "assets/abtlinelight.png";
+      abt_bg.src = theme === "light" ? "assets/abtbg.png" : "assets/abtbg.png";
 
   } else {
     document.body.classList.toggle("dark-theme");
@@ -31,6 +34,7 @@ btn.addEventListener("click", function () {
       : "light";
       abt_btm_img.src = theme === "dark" ? "assets/abtbottomlite.png" : "assets/aboutBottom.png";
       abt_line.src = theme === "dark" ? "assets/abtlinelight.png" : "assets/abtline.png";
+      abt_bg.src = theme === "dark" ? "assets/abtbg-dark.png" : "assets/abtbg-dark.png";
 
   }
   localStorage.setItem("theme", theme);
